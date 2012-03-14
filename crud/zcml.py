fa94f165e1ed8d47cde6dba5144ca3297cbba08f
sergey.volobuev <sergey@mooball.com>
2012-03-14 12:02:16 +1000

    removed zcml stuff

--- crud/zcml.py
+++ crud/zcml.py
@@ -1,84 +1,84 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 
-##########################################
-#     This file forms part of CRUD
-#     Copyright: refer to COPYRIGHT.txt
-#     License: refer to LICENSE.txt
-##########################################
-
-
-from zope.interface import Interface
-from zope.configuration.xmlconfig import include, includeOverrides
-from zope.configuration.fields import GlobalObject
-from zope.schema import BytesLine, TextLine
+###########################################
+##     This file forms part of CRUD
+##     Copyright: refer to COPYRIGHT.txt
+##     License: refer to LICENSE.txt
+###########################################
 
 
-class IRegisterDirective(Interface):
-    """
-    register directive registers an SA model with crud's Resource so that resource is
-    instantiated when, for example, URL traversal is happening
-    """
+#from zope.interface import Interface
+#from zope.configuration.xmlconfig import include, includeOverrides
+#from zope.configuration.fields import GlobalObject
+#from zope.schema import BytesLine, TextLine
 
 
-    model = GlobalObject(
-        title=u"Crud model",
-        description=u""" """,
-        required=True,
-        )
+#class IRegisterDirective(Interface):
+    #"""
+    #register directive registers an SA model with crud's Resource so that resource is
+    #instantiated when, for example, URL traversal is happening
+    #"""
 
 
-    resource = GlobalObject(
-        title=u"Crud resource",
-        description=u""" """,
-        required=True,
-        )
-
-def register(_context, model, resource):
-    """
-    registers an SA model with crud
-    <crud:register model=".models.Client" resource=".proxies.ClientResource" />
-
-    You can use ZCML or, alternatively, use the @resource decorator (see registry.py)
-    """
-    import crud
-    crud.register(model, resource)
+    #model = GlobalObject(
+        #title=u"Crud model",
+        #description=u""" """,
+        #required=True,
+        #)
 
 
-class ICollectionDirective(Interface):
-    """
-    crud directive registers a CRUD section
-    """
+    #resource = GlobalObject(
+        #title=u"Crud resource",
+        #description=u""" """,
+        #required=True,
+        #)
 
-    title = TextLine(
-        title=u"Title",
-        description=(u""),
-        required=True,
-        )
+#def register(_context, model, resource):
+    #"""
+    #registers an SA model with crud
+    #<crud:register model=".models.Client" resource=".proxies.ClientResource" />
 
-    slug = TextLine(
-        title=u"Slug",
-        description=(u""),
-        required=True,
-        )
+    #You can use ZCML or, alternatively, use the @resource decorator (see registry.py)
+    #"""
+    #import crud
+    #crud.register(model, resource)
 
-    section = GlobalObject(
-        title=u"Crud object",
-        description=u""" """,
-        required=True,
-        )
 
-    model = GlobalObject(
-        title=u"Crud object",
-        description=u""" """,
-        required=False, # Someitmes we want Collections without any subitems
-        )
+#class ICollectionDirective(Interface):
+    #"""
+    #crud directive registers a CRUD section
+    #"""
 
-def section(_context, slug, title, section, model=None):
-    """ registers a CRUD section """
-    import crud
+    #title = TextLine(
+        #title=u"Title",
+        #description=(u""),
+        #required=True,
+        #)
 
-    root = crud.get_root()
+    #slug = TextLine(
+        #title=u"Slug",
+        #description=(u""),
+        #required=True,
+        #)
 
-    # Avoid assigning stuff to the class attribute
-    if root is not None:
-        root.__dict__.setdefault('subsections', {})[slug] = section(title, model)
+    #section = GlobalObject(
+        #title=u"Crud object",
+        #description=u""" """,
+        #required=True,
+        #)
+
+    #model = GlobalObject(
+        #title=u"Crud object",
+        #description=u""" """,
+        #required=False, # Someitmes we want Collections without any subitems
+        #)
+
+#def section(_context, slug, title, section, model=None):
+    #""" registers a CRUD section """
+    #import crud
+
+    #root = crud.get_root()
+
+    ## Avoid assigning stuff to the class attribute
+    #if root is not None:
+        #root.__dict__.setdefault('subsections', {})[slug] = section(title, model)
